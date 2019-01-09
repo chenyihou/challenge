4c17b215d3a95660f6d7d3d1a7f508d68ca8996a
Chenyi Hou <chenyi.hou88@gmail.com>
2019-01-09 08:58:46 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,22 +2,21 @@ const data = require('./data.json')
 const express = require('express')
 const app = express()
 
-let dict = new Map() // this is for the response of certain keyword, key is keyword, value is object
-let hashmap=new Map() // this is for all objects in data, key is title, value is object
+let dict = new Map() // this hashmap is to quickly find requested objects based on certain keyword
+let hashmap=new Map() // this hashmap is to quickly located the item when the user wants to mark the article
 
 
 data.forEach(d => {
     d.marked=false
-    const keywords = d.keywords.split(', ')
+    const keywords = d.keywords.split(', ') 
     const {title}=d
-    hashmap.set(title,d)
+    hashmap.set(title,d) // the key value pair would look like {title: {result}}
     keywords.forEach(keyword => {
         keys = keyword.split(' ')
         keys.forEach(key => {
-            key=key.replace('(','').replace(')','')
+            key=key.replace('(','').replace(')','') // the key value pair would look like {key: { {result1}, {result2}, ..., {resultN} }}
             let value = dict.get(key) ? dict.get(key) : new Set()
             value.add(d)
-            // console.log(value)
             dict.set(key, value)
         })
     })
@@ -25,10 +24,10 @@ data.forEach(d => {
 
 
 app.use(express.static('public'))
+
 app.get('/api', (req, res) => {
-    const {search ,mark} = req.query
-    // console.log(!!dict.get(search), search)
-    if(search){
+    const {search, mark} = req.query
+    if(search){ // if this is a request for searching keyword related articles
         let response = dict.get(search) ? dict.get(search) : []
         let ret = []
         response.forEach(item => {
@@ -38,7 +37,7 @@ app.get('/api', (req, res) => {
             ret
         })
     }
-    else{
+    else{ // if this is a request for marking certain articles
         let markObj=hashmap.get(mark)
         if(markObj){
             markObj.marked=!markObj.marked
